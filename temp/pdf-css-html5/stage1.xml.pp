bf1f735954e4a252f95077cd47ae7198bbe45af0
Baniolka <151279058+Baniolka@users.noreply.github.com>
2024-06-07 23:05:01 +0200

    Baza projektu

---
+++ temp/pdf-css-html5/stage1.xml.pp
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?><map xmlns:css2fo="http://www.oxygenxml.com/css2fo" xmlns:dita-ot="http://dita-ot.sourceforge.net/ns/201007/dita-ot" xmlns:ditaarch="http://dita.oasis-open.org/architecture/2005/" xmlns:nd="http://www.oxygenxml.com/css2fo/named-destinations" xmlns:opentopic="http://www.idiominc.com/opentopic" xmlns:opentopic-index="http://www.idiominc.com/opentopic/index" xmlns:oxy="http://www.oxygenxml.com/extensions/author" xmlns:xs="http://www.w3.org/2001/XMLSchema" cascade="merge" class="- map/map " ditaarch:DITAArchVersion="1.3" domains="(map mapgroup-d) (topic abbrev-d) (topic delay-d) a(props deliveryTarget) (map ditavalref-d) (map glossref-d) (topic hazard-d) (topic hi-d) (topic indexing-d) (topic markup-d) (topic pr-d) (topic relmgmt-d) (topic sw-d) (topic ui-d) (topic ut-d) (topic markup-d xml-d)" specializations="@props/deliveryTarget" xtrc="map:1;3:6" xtrf="file:/C:/New%20Folder/rozdzial_1.ditamap" xml:lang="en"><oxy:front-page class="- front-page/front-page "><oxy:front-page-title class="- front-page/front-page-title "><title xmlns:ot-placeholder="http://suite-sol.com/namespaces/ot-placeholder" class="- topic/title ">rozdzial_1</title></oxy:front-page-title></oxy:front-page><opentopic:map xmlns:ot-placeholder="http://suite-sol.com/namespaces/ot-placeholder" class="- toc/toc "><oxy:toc-title class="- toc/title " empty="true"/>
+    <title class="- topic/title ">rozdzial_1</title>
+    <topicref is-chapter="true" class="- map/topicref " href="#unique_1" type="topic"><topicmeta class="- map/topicmeta " data-topic-id="_1_zakladam_topic"><resourceid appid="_1_zakladam_topic" class="- topic/resourceid " oxy-source="topic"/><navtitle href="#unique_1" class="- topic/navtitle ">I'm Starting a Startup!</navtitle><?ditaot gentext?></topicmeta></topicref>
+</opentopic:map><topic break-before="true" is-chapter="true" class="- topic/topic " id="unique_1" topicrefclass="- map/topicref " nd:nd-id="_1_zakladam_topic" oid="_1_zakladam_topic" specializations="@props/deliveryTarget">
+    <title class="- topic/title "><ph class="- topic/ph topic/title-wrapper ">I'm Starting a Startup!</ph></title>
+    <body class="- topic/body ">
+        <p class="- topic/p ">Yes, the day has finally come to shout it out to everyone, or at least let your family and friends know, that you've got a brilliant idea and you're diving into business. You're starting a startup! You’ll be like Elon Musk or Mark Zuckerberg... some basement or garage will also be found... You can already picture the investors and customers lining up for your product, competitors in tears, fame, and glamour... WHOA, WHOA... hold your horses.</p>
+        
+        <p class="- topic/p ">To make all that a reality, you need knowledge, a plan, time, and some honest self-reflection. This guide will help you get a grip on what a startup is, its pros and cons, the qualities you need as a founder, the steps to take, and the methods available.</p>
+        
+        <p class="- topic/p ">It's a long journey from an idea to a product, so grab a notebook and let's dive into this guide together.</p>
+        
+        <note class="- topic/note ">
+            <p class="- topic/p "><b class="+ topic/ph hi-d/b ">A LITTLE NOTE!</b></p>
+            <p class="- topic/p ">Ladies, I've used the male form to simplify communication. This isn't just by chance... The statistics aren't in our favour when it comes to founding startups. Research shows that the two most common types of founders are men:</p>
+        </note>
+        
+        <section class="- topic/section ">
+            <title class="- topic/title ">Young startup founder (20-40 years old), who has:</title>
+            <ul class="- topic/ul ">
+                <li class="- topic/li "><p class="- topic/p ">a positive attitude</p></li>
+                <li class="- topic/li "><p class="- topic/p ">openness to cooperation with others</p></li>
+                <li class="- topic/li "><p class="- topic/p ">ability to set goals</p></li>
+                <li class="- topic/li "><p class="- topic/p ">not afraid to ask for feedback</p></li>
+            </ul>
+        </section>
+        
+        <section class="- topic/section ">
+            <title class="- topic/title ">Old startup founder (&gt;40 years old), who has:</title>
+            <ul class="- topic/ul ">
+                <li class="- topic/li "><p class="- topic/p ">experience in marketing and sales</p></li>
+                <li class="- topic/li "><p class="- topic/p ">project management skills</p></li>
+                <li class="- topic/li "><p class="- topic/p ">ability to select people for the team</p></li>
+                <li class="- topic/li "><p class="- topic/p ">knowledge of how to raise money for investments</p></li>
+            </ul>
+        </section>
+        
+        <p class="- topic/p ">Trends, like everything else, change. There are more and more women stepping up, but knowledge stays neutral. So, let's take this journey together.</p>
+    </body>
+</topic></map>
